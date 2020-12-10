61a25b20eba9c14e01d270a449a35eed3d40528a
Lucho Patiño <2450@holbertonschool.com>
2020-12-10 02:58:49 +0000

    File Creation

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for letter in str:
+        if 97 <= ord(letter) <= 122:
+            msg = chr(ord(letter) - 32)
+        else:
+            msg = letter
+        print(msg, end="")
+    print("")
