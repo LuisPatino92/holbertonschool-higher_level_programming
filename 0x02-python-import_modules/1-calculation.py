5905d8e216c304d0c520aa936d762771e8bec1c7
Lucho Patiño <2450@holbertonschool.com>
2020-12-11 00:47:48 +0000

    Integer format fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ import calculator_1 as c
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, c.add(a, b)))
-    print("{} - {} = {}".format(a, b, c.sub(a, b)))
-    print("{} * {} = {}".format(a, b, c.mul(a, b)))
-    print("{} / {} = {}".format(a, b, c.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, c.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, c.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, c.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, c.div(a, b)))
