9a7b05e38b8720081e5c3fcad9e773f0979cbb70
Lucho Patiño <2450@holbertonschool.com>
2020-12-11 01:08:07 +0000

    Format output correction

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,16 +5,20 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
     elif not argv[2] in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     elif argv[2] == "+":
-        print(add(int(argv[1]), int(argv[3])))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+              add(int(argv[1]), int(argv[3]))))
     elif argv[2] == "-":
-        print(sub(int(argv[1]), int(argv[3])))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+              sub(int(argv[1]), int(argv[3]))))
     elif argv[2] == "*":
-        print(mul(int(argv[1]), int(argv[3])))
-    elif argv[2] == "/":
-        print(div(int(argv[1]), int(argv[3])))
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+              mul(int(argv[1]), int(argv[3]))))
+    elif argv[2] == "/" and int(argv[3]) != 0:
+        print("{} {} {} = {}".format(argv[1], argv[2], argv[3],
+              div(int(argv[1]), int(argv[3]))))
