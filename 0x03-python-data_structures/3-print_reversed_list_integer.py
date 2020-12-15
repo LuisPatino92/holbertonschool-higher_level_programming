78e4edda7700676926940c4941021cb2ca741c4f
Lucho Patiño <2450@holbertonschool.com>
2020-12-15 19:18:03 +0000

    Empty list handled

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,3 +4,5 @@ def print_reversed_list_integer(my_list=[]):
     copy_list.reverse()
     for element in copy_list:
         print("{:d}".format(element))
+    if len(my_list) == 0:
+        print("")
