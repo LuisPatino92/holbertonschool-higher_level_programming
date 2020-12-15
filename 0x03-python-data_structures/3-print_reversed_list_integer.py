786b8eb2a796acd382ca96e89ecbd504bee68a97
Lucho Patiño <2450@holbertonschool.com>
2020-12-15 19:14:52 +0000

    Integer format correction

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@ def print_reversed_list_integer(my_list=[]):
     copy_list = my_list[:]
     copy_list.reverse()
     for element in copy_list:
-        print("{}".format(element))
+        print("{:d}".format(element))
