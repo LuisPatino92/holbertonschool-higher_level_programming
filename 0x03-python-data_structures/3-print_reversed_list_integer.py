b808cbc995ca3cc30d27653686a842bf7e2c988a
Lucho Patiño <2450@holbertonschool.com>
2020-12-15 19:22:39 +0000

    Reversing original list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    copy_list = my_list[:]
-    copy_list.reverse()
-    for element in copy_list:
+    my_list.reverse()
+    for element in my_list:
         print("{:d}".format(element))
-    if len(my_list) == 0:
-        print("")
