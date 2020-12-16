d0f0ed6c6c21f5e27f0dd914007c87ab68a025b1
Lucho Patiño <2450@holbertonschool.com>
2020-12-16 01:16:43 +0000

    Changing For functions in provided lybraries

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,12 +1,33 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <listobject.h>
+#include <object.h>
 
-void print_python_list_info(PyListObject list)
+/**
+ * print_python_list_info -  prints some basic info about Python lists.
+ * @p: The pointer to generic PyObject which is of PyListObject type
+ * Return: void
+ */
+void print_python_list_info(PyObject *list)
 {
+	PyObject *element;
 	int i;
 
-	printf("[*] Size of the Python List = %d\n", (int)list.ob_size);
-	printf("[*] Allocated = %d\n", (int)list.allocated);
-	for (i = 0; i < (int)list.ob_size; i++)
-		printf("Element %d: %s\n", i, (char *)Py_TYPE(list));
+	PyListObject *aux = (PyListObject *)list;
+
+	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(list));
+	printf("[*] Allocated = %li\n", aux->allocated);
+	for (i = 0; i < PyList_Size(list); i++)
+	{
+		element = PyList_GetItem(list, i);
+		if (PyFloat_Check(element))
+			printf("Element %d: float\n", i);
+		if (PyTuple_Check(element))
+			printf("Element %d: tuple\n", i);
+		if (PyList_Check(element))
+			printf("Element %d: list\n", i);
+		if (PyLong_Check(element))
+			printf("Element %d: int\n", i);
+		if (PyUnicode_Check(element))
+			printf("Element %d: str\n", i);
+	}
 }
