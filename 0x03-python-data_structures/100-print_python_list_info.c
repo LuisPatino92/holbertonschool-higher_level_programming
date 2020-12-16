dd8a5500b111caa194a337c6c417f5aed73198c1
Lucho Patiño <2450@holbertonschool.com>
2020-12-16 01:38:10 +0000

    Try with Python.h library 3

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,7 +7,6 @@
  */
 void print_python_list_info(PyObject *list)
 {
-	PyObject *element;
 	int i;
 
 	printf("[*] Size of the Python List = %d\n", (int)PyList_Size(list));
@@ -15,17 +14,15 @@ void print_python_list_info(PyObject *list)
 
 	for (i = 0; i < PyList_Size(list); i++)
 	{
-		element = PyList_GetItem(list, i);
-
-		if (PyFloat_Check(element))
+		if (PyFloat_Check(PyList_GetItem(list, i)))
 			printf("Element %d: float\n", i);
-		if (PyTuple_Check(element))
+		if (PyTuple_Check(PyList_GetItem(list, i)))
 			printf("Element %d: tuple\n", i);
-		if (PyList_Check(element))
+		if (PyList_Check(PyList_GetItem(list, i)))
 			printf("Element %d: list\n", i);
-		if (PyLong_Check(element))
+		if (PyLong_Check(PyList_GetItem(list, i)))
 			printf("Element %d: int\n", i);
-		if (PyUnicode_Check(element))
+		if (PyUnicode_Check(PyList_GetItem(list, i)))
 			printf("Element %d: str\n", i);
 	}
 }
