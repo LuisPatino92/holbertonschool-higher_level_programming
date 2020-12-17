05d55725ecf2ea196d095c19dce4373a0f9d5298
Lucho Patiño <2450@holbertonschool.com>
2020-12-17 22:35:57 +0000

    Pep 8 correction

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    new_list = list(map(lambda element:
-                    list(map(lambda num: num ** 2, element)), matrix))
-    return new_list
+def square_matrix_map(m=[]):
+    return list(map(lambda e: list(map(lambda n: n ** 2, e)), m))
