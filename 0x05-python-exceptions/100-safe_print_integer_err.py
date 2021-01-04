235ba7f11ddb972551fe8127adfcb20e6ddc8142
Lucho Patiño <2450@holbertonschool.com>
2021-01-04 20:09:56 +0000

    Placing the statement outside the try

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        return True
     except (ValueError, TypeError) as error_msg:
         print("Exeption: {}".format(error_msg), file=sys.stderr)
         return False
+    return True
