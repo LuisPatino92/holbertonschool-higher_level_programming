b0cb7a7b15d605bdbc8f0b618c419400801aaa09
Lucho Patiño <2450@holbertonschool.com>
2021-01-06 15:09:12 +0000

    Changing the decorator name and improving the printer

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -85,10 +85,9 @@ class Square:
     def my_print(self):
         """ Prints a square filling # pattern according to size"""
 
-        for _ in range(self.__position[1]):
-            print("")
-
         if self.__size > 0:
+            for _ in range(self.__position[1]):
+                print("")
             for i in range(self.__size):
                 print("{}{}".format(" " * (self.__position[0]),
                                     "#" * self.__size))
