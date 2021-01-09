2f19e106fe8eb8e50a361deab7fe4c6b46369d38
Lucho Patiño <2450@holbertonschool.com>
2021-01-09 17:49:22 +0000

    Wrong test case removed

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -11,5 +11,5 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1, -2, 10, 5]), 10)
         self.assertEqual(max_integer([]), None)
         self.assertEqual(max_integer([-1000]), -1000)
-        self.assertEqual(max_integer([1, -2, 3, -4, 5, -6, 7, -8, 9, 10, -152, 2, 2, 588]), -2)
+        self.assertEqual(max_integer([1, -2, 3, -4, 5, -6, 7, -8, 9, 10, -152, 2, 2, 588]), 588)
         self.assertEqual(max_integer([x for x in range(-40, 500, 2)]), 498)
