d3daea6f83e4ed19888f78f3188b9b0ffe724fe7
Lucho Patiño <2450@holbertonschool.com>
2021-01-20 02:11:31 +0000

    BaseGeometry empty class added

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module has The class BaseGeometry (Empty)"""
+
+
+class BaseGeometry():
+    """Empty class for rectangle and square modeling"""
+    pass
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-test.txt")
