5c02086a553a3caefe7a7f46f3ae3af798aa0289
Lucho Patiño <2450@holbertonschool.com>
2021-01-20 04:41:02 +0000

    Checking for dict in dir

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module has the add attribute function"""
+
+
+def add_attribute(obj, attr, value):
+    """Function that adds an attribute to an object if it is possible
+    Args:
+        obj:     The object to add the attribute in
+        attr:       The attribute
+        value:      The value to be asigned to the attribute
+    """
+
+    if "__dict__" not in list(map(str, dir(type(obj)))):
+        raise TypeError("can't add new attribute ")
+
+    obj.__dict__[attr] = value
