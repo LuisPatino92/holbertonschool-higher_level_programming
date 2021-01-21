6eb1bfcf49083a7870d4a1f79d98f9cdc880d5fd
Lucho Patiño <2450@holbertonschool.com>
2021-01-21 03:54:20 +0000

    Without json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """This module has the class_to_json function"""
 
-
-import json
-
-
 def class_to_json(obj):
     """Returns the dictionary description for JSON of an object"""
 
-    serial = json.loads(json.dumps(obj.__dict__))
+    serial = obj.__dict__
 
     return serial
