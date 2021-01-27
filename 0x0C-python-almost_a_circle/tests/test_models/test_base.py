afc8f1ef5893768d7df4f9aadf0819e02e95eb82
Lucho Patiño <2450@holbertonschool.com>
2021-01-27 04:54:45 +0000

    First tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -4,13 +4,13 @@
 
 import unittest
 import pep8
+from models.base import Base
 
 
 class BaseTests(unittest.TestCase):
     """Tests for Base class"""
 
     def test_pep8_main_file(self):
-        """Test pep8 ok."""
         """Test pep8 ok."""
         r = pep8.StyleGuide().check_files(['models/base.py'])
         self.assertEqual(r.total_errors, 0, "Pep8 issues Found.")
@@ -19,6 +19,10 @@ class BaseTests(unittest.TestCase):
         """Test pep8 ok."""
         r = pep8.StyleGuide().check_files(['tests/test_models/test_base.py'])
         self.assertEqual(r.total_errors, 0, "Pep8 issues Found.")
+
+    def test_docs(self):
+        """Test for Base docstring"""
+        self.assertTrue(len(Base.__doc__) > 1)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""unitary test for Base Class"""
+
+
+import unittest
+import pep8
+
+
+class RectangleTests(unittest.TestCase):
+    """Tests for Base class"""
+
+    def test_pep8_main_file(self):
+        """Test pep8 ok."""
+        """Test pep8 ok."""
+        r = pep8.StyleGuide().check_files(['models/rectangle.py'])
+        self.assertEqual(r.total_errors, 0, "Pep8 issues Found.")
+
+    def test_pep8_test(self):
+        """Test pep8 ok."""
+        r = pep8.StyleGuide().check_files(
+                ['tests/test_models/test_rectangle.py'])
+        self.assertEqual(r.total_errors, 0, "Pep8 issues Found.")
+
+if __name__ == "__main__":
+    unittest.main()
