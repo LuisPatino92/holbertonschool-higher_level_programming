6a7c70931b75ee830395b594e94c351af18f8d60
Lucho <2450@holbertonschool.com>
2021-03-02 14:05:33 -0500

    Using CREAT along with IF NOT EXISTS

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- This script creates a new table in the current BD
+-- The database name should be especified in the calling of the script
+
+CREATE TABLE IF NOT EXISTS first_table (
+	id INTEGER,
+	name VARCHAR(256)
+);
