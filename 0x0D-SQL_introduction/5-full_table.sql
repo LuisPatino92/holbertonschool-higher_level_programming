d7f587aaa115c1fcf043c947b26495f8274978af
Lucho <2450@holbertonschool.com>
2021-03-02 15:09:51 -0500

    Using CREATE TABLE to show original CREATE statement

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Prints the full description of the table first_table from the DB hbtn_0c_0
 -- The database name should be especified in the calling of the script
 
-DESC first_table;
+SHOW CREATE TABLE first_table;
