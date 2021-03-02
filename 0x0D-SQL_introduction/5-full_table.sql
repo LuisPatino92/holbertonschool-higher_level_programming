4d6d430ea1487b3619dafe956a049a361a56aab4
Lucho <2450@holbertonschool.com>
2021-03-02 14:28:23 -0500

    Using Show column command

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Prints the full description of the table first_table from the DB hbtn_0c_0
+-- The database name should be especified in the calling of the script
+
+SHOW FULL COLUMNS FROM first_table;
