db1f15aba9d39c1924e316c4b146c8f96cd224bd
Lucho <2450@holbertonschool.com>
2021-03-02 18:34:58 -0500

    Using COUNT function

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Display the number of records with id = 89 in first_table of hbtn_0c_0
+-- The database name should be especified in the calling of the script
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
