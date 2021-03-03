4c806a342e870547f4fcfe63ee6fa427d3aedf2a
Lucho <2450@holbertonschool.com>
2021-03-03 18:34:57 -0500

    Using alter command

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Changes DB encoding to utf8
+-- The database name should be especified in the calling of the script
+
+ALTER DATABASE hbtn_0c_0 charset=utf8;
+USE hbtn_0c_0;
+ALTER TABLE first_table charset=utf8;
