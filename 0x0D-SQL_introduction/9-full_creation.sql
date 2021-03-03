bca4d2b511bf700884d4fc134b3996918ffbd508
Lucho <2450@holbertonschool.com>
2021-03-02 19:05:57 -0500

    Corrected the name of Jhon... Was John

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,13 +2,13 @@
 -- The database name should be especified in the calling of the script
 
 CREATE TABLE IF NOT EXISTS second_table(
-	id INTEGER,
+	`id` INTEGER,
 	`name` VARCHAR(256),
-	score INTEGER
+	`score` INTEGER
 );
 
-INSERT INTO second_table (id, `name`, score) VALUES
-	(1, "Jhon", 10),
+INSERT INTO second_table (`id`, `name`, `score`) VALUES
+	(1, "John", 10),
 	(2, "Alex", 3),
 	(3, "Bob", 14),
 	(4, "George", 8);
