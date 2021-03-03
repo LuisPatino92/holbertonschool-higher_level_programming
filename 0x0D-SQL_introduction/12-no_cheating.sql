38b0556003c027d12747a7412d2a148e956d70ac
Lucho <2450@holbertonschool.com>
2021-03-02 22:57:27 -0500

    Using UPDATE function for all rows with name = Bob

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updates the score of Bob to 10 in the table second_table
+-- The database name should be especified in the calling of the script
+
+UPDATE second_table SET `score` = 10 WHERE `name` = "Bob";
