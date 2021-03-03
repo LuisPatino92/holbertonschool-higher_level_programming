cf299c8583c766581151f96a072b364bbd4b05a5
Lucho <2450@holbertonschool.com>
2021-03-02 19:21:01 -0500

    Using SELECT, ORDER BY and DESC

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- Lists all the records of second_table ordered by score
+-- The database name should be especified in the calling of the script
+
+SELECT `score`, `name` FROM second_table ORDER BY `score` DESC; 
