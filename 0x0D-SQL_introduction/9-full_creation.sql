aeef78319220040c6a079e5131468d429c3c298b
Lucho <2450@holbertonschool.com>
2021-03-02 19:03:31 -0500

    Remove some backtilts

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,12 +2,12 @@
 -- The database name should be especified in the calling of the script
 
 CREATE TABLE IF NOT EXISTS second_table(
-	`id` INTEGER,
+	id INTEGER,
 	`name` VARCHAR(256),
-	`score` INTEGER
+	score INTEGER
 );
 
-INSERT INTO second_table (`id`, `name`, `score`) VALUES
+INSERT INTO second_table (id, `name`, score) VALUES
 	(1, "Jhon", 10),
 	(2, "Alex", 3),
 	(3, "Bob", 14),
