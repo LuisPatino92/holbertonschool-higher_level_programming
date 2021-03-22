c5fbad325bd687ab42b95bc43b39d71d9eb89b13
Lucho <2450@holbertonschool.com>
2021-03-22 13:46:17 -0500

    Using isNaN to check

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
 
-console.log(`${process.argv[2]}`, 'is', `${process.argv[3]}`);
+if (isNaN(process.argv[2])) console.log('Not a number');
+else console.log('My number:', `${process.argv[2]}`);
