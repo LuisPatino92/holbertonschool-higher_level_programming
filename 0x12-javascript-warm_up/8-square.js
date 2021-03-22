0c0e263a5775531e31773a1ca4c70771028a8f59
Lucho <2450@holbertonschool.com>
2021-03-22 14:07:59 -0500

    Handling negative numbers

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const repeats = Number(process.argv[2]);
 
 if (isNaN(repeats)) {
   console.log('Missing number of occurrences');
-} else {
+} else if (repeats > 0) {
   for (let i = 0; i < repeats; i++) {
     console.log('X'.repeat(repeats));
   }
