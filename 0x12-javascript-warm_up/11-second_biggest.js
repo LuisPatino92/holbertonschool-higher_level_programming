5ff4aba01811381f47c1a4e46f45e6b61d3d28a9
Lucho <2450@holbertonschool.com>
2021-03-22 15:34:45 -0500

    Using a huge pipeline

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 if (process.argv.length < 4) console.log(`${0}`);
-else console.log(`${Math.max.apply(Math, process.argv.slice(2))}`);
+else console.log(`${process.argv.slice(2).sort()[process.argv.slice(2).length - 2]}`);
