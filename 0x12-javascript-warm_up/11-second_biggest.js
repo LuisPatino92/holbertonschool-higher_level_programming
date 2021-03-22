f6c4d4b009d6530d9a2198ab92ec695a35a52f2b
Lucho <2450@holbertonschool.com>
2021-03-22 15:37:16 -0500

    Using Number constructor

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 if (process.argv.length < 4) console.log(`${0}`);
-else console.log(`${process.argv.slice(2).sort()[process.argv.slice(2).length - 2]}`);
+else console.log(`${Number(process.argv.slice(2).sort()[process.argv.slice(2).length - 2])}`);
