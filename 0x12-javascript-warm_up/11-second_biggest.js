5c4702b24d320035eb24b75ae93c682c66ca146f
Lucho <2450@holbertonschool.com>
2021-03-22 14:47:04 -0500

    Using a conversor of format to meet requirements

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-if (isNaN(Number(process.argv[2]))) console.log(0);
+if (process.argv.length < 4) console.log(`${0}`);
 else console.log(`${Math.max.apply(Math, process.argv.slice(2))}`);
