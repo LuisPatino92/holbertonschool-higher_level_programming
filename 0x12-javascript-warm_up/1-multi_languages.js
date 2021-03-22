290dd991c912583c0d1233278a19a5650691b1c5
Lucho <2450@holbertonschool.com>
2021-03-22 12:38:03 -0500

    Adding semmicolons to fit STandard

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const msgArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const msgArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < msgArray.length; i++) {
-  console.log(msgArray[i])
+  console.log(msgArray[i]);
 }
