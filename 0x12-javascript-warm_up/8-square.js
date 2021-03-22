7637dfb9a82e63cd7e95a432ffbd96d85eb28605
Lucho <2450@holbertonschool.com>
2021-03-22 14:13:31 -0500

    Message changed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const repeats = Number(process.argv[2]);
 
 if (isNaN(repeats)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else if (repeats > 0) {
   for (let i = 0; i < repeats; i++) {
     console.log('X'.repeat(repeats));
