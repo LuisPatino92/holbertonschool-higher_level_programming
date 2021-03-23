5e58a8c810744948bea6f5343a34da4a3d621e3f
Lucho <2450@holbertonschool.com>
2021-03-23 14:08:48 -0500

    Manual swaping aproach

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse()
+  let aux;
+  for (let i = 0; i < Math.floor(list.length / 2); i++) {
+    aux = list[i];
+    list[i] = list[list.length - i - 1];
+    list[list.length - i - 1] = aux;
+  }
+  return (list)
 };
