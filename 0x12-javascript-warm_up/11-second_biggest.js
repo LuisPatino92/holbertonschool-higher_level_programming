9b2c6ac774b99b84b84281a84289a74473fc24ac
Lucho <2450@holbertonschool.com>
2021-03-24 11:22:07 -0500

    Using advanced concepts

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let sorted = new Float64Array(process.argv.slice(2)).sort();
+const sorted = new Float64Array(process.argv.slice(2)).sort();
 
-if (process.argv.length < 4) console.log(`${0}`);
+if (process.argv.length < 4) console.log(0);
 else console.log(`${Number(sorted[sorted.length - 2])}`);
