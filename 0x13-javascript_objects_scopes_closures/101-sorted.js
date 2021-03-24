aef44440958dacc3c846b3b1aa42b766c00b5026
Lucho <2450@holbertonschool.com>
2021-03-23 21:33:58 -0500

    Changed let to const due never reasigned

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -4,7 +4,7 @@ const originalDict = require('./101-data').dict;
 
 const newObj = {};
 
-for (let element in originalDict) {
+for (const element in originalDict) {
   if (newObj[originalDict[element]] === undefined) {
     newObj[originalDict[element]] = [];
   }
