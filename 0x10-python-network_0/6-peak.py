54cf2d5c13131105b5c486bc92e8ff8358ef55b8
LuisPatino92 <2450@holbertonschool.com>
2021-04-08 18:53:02 -0500

    Using a sort with Log complexity II

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,4 +5,9 @@
 def find_peak(list_of_integers):
     """Returns the peak of a list """
 
-    return list_of_integers.sort()[-1]
+    if len(list_of_integers) == 0:
+        a = None
+    else:
+        a =list_of_integers.sort()[-1]
+
+    return a
