d2ea579457df28cab42490434f9d03ae81fa11ed
LuisPatino92 <2450@holbertonschool.com>
2021-04-08 16:08:01 -0500

    Not asking for options II

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This scripts prints the body of a get following all the redirections
-curl -I "$1" 2> dev/null | grep  "Allow: " | cut -d " " -f 2-5
+curl -I "$1" 2> dev/null | grep Allow | cut -d " " -f 2-5
