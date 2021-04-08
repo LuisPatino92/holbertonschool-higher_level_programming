0108a517332648075591de8ce4ec9ca35a99bfd5
LuisPatino92 <2450@holbertonschool.com>
2021-04-08 16:39:51 -0500

    Using a conditional to avoid math errors

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,11 @@
 """This module has the find_peak function"""
 
 def find_peak(list_of_integers):
-    """Returns the peak of a list"""
+    """Returns the peak of a list """
 
-    return max(list_of_integers)
+    if len(list_of_integers) == 0:
+        a = None
+    else:
+        a = max(list_of_integers)
+
+    return a
