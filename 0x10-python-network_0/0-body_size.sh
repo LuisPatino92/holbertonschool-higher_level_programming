3707d43abc1b5ab725ddecef3817b5800650fdb1
LuisPatino92 <2450@holbertonschool.com>
2021-04-08 15:08:29 -0500

    Correction with a variable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This scripts prints the length of the body of a HTTP response
-expr $(echo $(curl 0.0.0.0:5000 2> /dev/null) | wc -c) - 1
+expr $(echo $(curl "$1" 2> /dev/null) | wc -c) - 1
