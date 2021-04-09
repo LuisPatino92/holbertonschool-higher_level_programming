57bddf97bcdfd3dd207ad6b8fab48da62397526c
LuisPatino92 <2450@holbertonschool.com>
2021-04-08 21:54:03 -0500

    using -d to send parameters III

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Post with body parameters
-curl -d 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1" 2> dev/null
+curl -sd 'email=hr@holbertonschool.com&subject=I will always be here for PLD' "$1"
