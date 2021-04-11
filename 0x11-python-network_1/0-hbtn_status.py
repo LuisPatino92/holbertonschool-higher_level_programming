b39a8cd5db30fc602b8f8426d19032fb0bace29b
LuisPatino92 <2450@holbertonschool.com>
2021-04-11 17:21:42 +0000

    Adding headers

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as url_r:
         response = url_r.read()
         print("Body response:")
-        print("\t- {}".format(type(response)))
-        print("\t- {}".format(response))
-        print("\t- {}".format(str(response, 'utf-8')))
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(str(response, 'utf-8')))
