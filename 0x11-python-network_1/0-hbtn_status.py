0db53060cbc731c1ac74333b9e4ba00f258c2849
LuisPatino92 <2450@holbertonschool.com>
2021-04-11 17:20:04 +0000

    using with statement to acomplish requirement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,9 +5,9 @@
 import urllib.request
 
 if __name__ == "__main__":
-    url_r = urllib.request.urlopen('https://intranet.hbtn.io/status')
-    response = url_r.read()
-    print("Body response:")
-    print("\t- {}".format(type(response)))
-    print("\t- {}".format(response))
-    print("\t- {}".format(str(response, 'utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as url_r:
+        response = url_r.read()
+        print("Body response:")
+        print("\t- {}".format(type(response)))
+        print("\t- {}".format(response))
+        print("\t- {}".format(str(response, 'utf-8')))
