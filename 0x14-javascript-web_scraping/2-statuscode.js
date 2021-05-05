6f09e2be74afe2436c28b252fe2864a9381e9c07
LuisPatino92 <2450@holbertonschool.com>
2021-05-04 21:22:19 -0500

    Semistandard fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 
-request(process.argv[2]).on('response', function(response) {
-  console.log("code:", response.statusCode)
-})
-
+request(process.argv[2]).on('response', function (response) {
+  console.log('code:', response.statusCode);
+});
